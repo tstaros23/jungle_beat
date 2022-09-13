bab68dc2f55534ebe36a90124d56ed9b3239c80e
Ted Staros <tstaros23@gmail.com>
2022-09-13 00:21:30 -0600

    Create test for appending data to a linked list

---
+++ spec/linked_list_spec.rb
@@ -0,0 +1,15 @@
+require "./lib/linked_list"
+
+RSpec.describe LinkedList do
+  before(:each) do
+    @list = LinkedList.new
+  end
+
+  it "can add a piece of data to a list" do
+    expect(@list.head).to eq(nil)
+    @list.append("doop")
+    expect(@list.head.next_node).to eq(nil)
+    expect(@list.count).to eq(1)
+    expect(@list.to_string).to eq("doop")
+  end
+end
